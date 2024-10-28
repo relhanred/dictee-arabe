bc126aa572fce3860ebfdd155633170792f5f19c
Redha El hanti <relhanti@gmail.com>
2024-10-29 00:34:23 +0100

    fix: eslint escape

--- app/connexion/page.jsx
+++ app/connexion/page.jsx
@@ -108,7 +108,7 @@ export default function SignIn() {
                 </form>
                 <div className="text-center">
                     <p className="text-sm text-gray-600">
-                        Don't have an account?{' '}
+                        Don&#39;t have an account?{' '}
                         <Link href="/signup" className="font-medium text-indigo-600 hover:text-indigo-500">
                             Sign up
                         </Link>
