d97fc940c0de5376bd144a4da33fd481e7c110d3
Redha El hanti <relhanti@gmail.com>
2024-11-18 01:54:04 +0100

    feat: min height on app layout

--- app/layout/AppLayout.jsx
+++ app/layout/AppLayout.jsx
@@ -7,7 +7,7 @@ const AppLayout = ({ children }) => {
             <div className="inline-flex items-center justify-center h-[6.5rem] bg-gray-900 w-full mb-auto">
                 <ImlaaIcon className="w-40" />
             </div>
-            <div className="p-6 flex-1 bg-gray-50">
+            <div className="min-h-[calc(100vh-6.5rem)] flex-1 bg-gray-50 p-6">
                 {children}
             </div>
         </main>
