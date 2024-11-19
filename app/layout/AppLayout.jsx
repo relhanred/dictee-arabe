f28dddd7f65782ef9ebd1fbb72919ccc6950ee0e
Redha El hanti <relhanti@gmail.com>
2024-11-19 02:56:41 +0100

    feat: app layout with flash provider

--- app/layout/AppLayout.jsx
+++ app/layout/AppLayout.jsx
@@ -1,16 +1,20 @@
+'use client'
 import React from 'react';
 import ImlaaIcon from "@/app/components/icons/ImlaaIcon";
+import { FlashProvider } from '@/app/contexts/FlashContext';
 
 const AppLayout = ({ children }) => {
     return (
-        <main className="flex flex-col min-h-screen">
-            <div className="inline-flex items-center justify-center h-[6.5rem] bg-gray-900 w-full mb-auto">
-                <ImlaaIcon className="w-40" />
-            </div>
-            <div className="min-h-[calc(100vh-6.5rem)] flex-1 bg-gray-50 p-6">
-                {children}
-            </div>
-        </main>
+        <FlashProvider>
+            <main className="flex flex-col min-h-screen">
+                <div className="inline-flex items-center justify-center h-[6.5rem] bg-gray-900 w-full mb-auto">
+                    <ImlaaIcon className="w-40" />
+                </div>
+                <div className="min-h-[calc(100vh-6.5rem)] flex-1 bg-gray-50 p-6">
+                    {children}
+                </div>
+            </main>
+        </FlashProvider>
     );
 };
 
