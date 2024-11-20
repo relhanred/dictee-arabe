68091ab81aa06db182bf42f597c6e453d942dcd3
Redha El hanti <relhanti@gmail.com>
2024-11-20 05:08:45 +0100

    css: update audio recorder

--- app/components/DictationForm.jsx
+++ app/components/DictationForm.jsx
@@ -187,9 +187,9 @@ const DictationForm = ({onSuccess,initialData = null }) => {
                                     <button
                                         type="button"
                                         onClick={handleEditAudio}
-                                        className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                                        className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-1 focus:ring-offset-1 focus:ring-gray-500"
                                     >
-                                        Modifier audio
+                                        Modifier
                                     </button>
                                 </div>
                             ) : (
@@ -217,7 +217,7 @@ const DictationForm = ({onSuccess,initialData = null }) => {
                                         <button
                                             type="button"
                                             onClick={handleCancelEditAudio}
-                                            className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                                            className="w-full flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-offset-1 focus:ring-1 focus:ring-gray-500"
                                         >
                                             Annuler la modification
                                         </button>
